a46b1791b8ab85fb8c162f9d1e20da0b16110960
Arthur Bomfim Neto <you@example.com>
2020-10-20 01:48:56 -0300

    Refatorado as rotas do rquivo users_controller.js com try/catch e async/await

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,76 +1,57 @@
-const mysql = require('../mysql').pool;
+const mysql = require('../mysql');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-exports.registerUsers = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) { return res.status(500).send({ error: error }) }
-        conn.query('SELECT * FROM users WHERE email = ?',
-            [req.body.email],
-            (error, result) => {
-                if (error) { return res.status(500).send({ error: error }) }
-                if (result.length > 0) {
-                    res.status(409).send({ message: 'User already registered' })
-                } else {
-                    bcrypt.hash(req.body.password, 10, (errBcrypt, hash) => {
-                        if (errBcrypt) { return res.status(500).send({ error: errBcrypt }) }
+exports.registerUsers = async (req, res, next) => {
 
-                        conn.query(
-                            `INSERT INTO users (email, password) VALUES(?, ?)`,
-                            [req.body.email, hash],
-                            (error, result) => {
-                                conn.release();
-                                if (error) { return res.status(500).send({ error: error }) }
-                                response = {
-                                    message: 'User created successfully',
-                                    usersCreated: {
-                                        id_users: result.insertId,
-                                        email: req.body.email
-                                    }
-                                }
-                                return res.status(201).send({ response })
-                            }
-                        )
-                    });
-                }
-            })
+    try {
+        const result = await mysql.execute('SELECT * FROM users WHERE email = ?', [req.body.email]);
+        const hash = await bcrypt.hashSync(req.body.password, 10);
 
-    });
+        query = 'INSERT INTO users (email, password) VALUES (?,?)';
+        const results = await mysql.execute(query, [req.body.email, hash]);
+
+        const response = {
+            message: 'User created successfull',
+            createdUser: {
+                userId: results.insertId,
+                email: req.body.email
+            }
+        }
+        return res.status(201).send(response);
+
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
 };
 
-exports.loginUsers = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) { return res.status(500).send({ error: error }) }
-        conn.query('SELECT * FROM users WHERE email = ?',
-        [req.body.email],
-        (error, results) => {
-            conn.release();
-            if (error) {return res.status(500).send({error: error})}
-            if (results.length < 1){
-                return res.status(401).send({ message: 'Authentication failure'})
-            }
-            bcrypt.compare(req.body.password, results[0].password, (error, result) => {
-                if (error){
-                    return res.status(401).send({ message: 'Authentication failure'}) 
-                }
-                if (result) {
-                    console.log(result.id_user)
-                    const token = jwt.sign({
-                        id_user: results[0].id_user,
-                        email: results[0].email 
-                    }, 
-                    process.env.JWT_KEY,
-                    {
-                        expiresIn: "10h"
-                    });
-                    return res.status(200).send({ 
-                        message : 'Successfully authenticated',
-                        token: token
-                    });
-                }
-                return res.status(401).send({ message: 'Authentication failure'})
-            });
-        });
-});
+exports.loginUsers = async (req, res, next) => {
 
+    try {
+        const query = `SELECT * FROM users WHERE email = ?`;
+        var results = await mysql.execute(query, [req.body.email]);
+
+        if (results.length < 1) {
+            return res.status(401).send({ message: 'Authentication failure' })
+        }
+
+        if (await bcrypt.compareSync(req.body.password, results[0].password)) {
+            const token = jwt.sign({
+                id_user: results[0].id_user,
+                email: results[0].email
+            },
+                process.env.JWT_KEY,
+                {
+                    expiresIn: "1h"
+                });
+            return res.status(200).send({
+                message: 'Successfully authenticated',
+                token: token
+            });
+        }
+        return res.status(401).send({ message: 'Authentication failure' })
+
+    } catch (error) {
+        return res.status(500).send({ message: 'Authentication failure' });
+    }
 };
